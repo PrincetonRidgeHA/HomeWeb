267d258cbbd09fcf1aa2d19df48e018b185f0470
Joshua Zenn <wordman05@gmail.com>
2015-09-10 15:27:15 -0400

    Update mailer.rb

--- inc/mailer.rb
+++ inc/mailer.rb
@@ -1,3 +1,12 @@
+require_once 'pony'
+
 module Mailer
-  
+  def send(String sender, String to, String subject, String body)
+    Pony.mail({
+      :from => sender,
+      :to => to,
+      :subject => "AUTO: PRHA bug report",
+      :body => body
+    })
+  end
 end
