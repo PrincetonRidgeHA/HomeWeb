f8a3a57b78e9fda307270863f8a7574fbe9c5c0a
Joshua Zenn <jzenn@students.kennesaw.edu>
2015-12-04 18:59:34 +0000

    WIP upload

--- inc/notifications.rb
+++ inc/notifications.rb
@@ -2,7 +2,6 @@ module Notifications
   def Notifications.get_all()
     rVal = Array.new
     rVal.push("Heads up! This site is a work in progess. Some things might not work the way they should.")
-    rVal.push("HEROKU: This build is on ring DEV-PUB, which is marked as unstable.")
     # Insert other global notifications here
     return rVal
   end
