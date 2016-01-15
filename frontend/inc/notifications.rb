93b7a99b7dd4546f263b48393d41f0dbb44f95d4
Joshua Zenn <jzenn@students.kennesaw.edu>
2016-01-15 20:51:05 +0000

    Removed WIP notification on production site

--- frontend/inc/notifications.rb
+++ frontend/inc/notifications.rb
@@ -1,7 +1,9 @@
 module Notifications
   def Notifications.get_all()
     rVal = Array.new
-    rVal.push("Heads up! This site is a work in progess. Some things might not work the way they should.")
+    if(ENV['HEROKU_PIPELINE'] == 'staging')
+      rVal.push("WARNING: You are using the development version of this site.")
+    end
     # Insert other global notifications here
     return rVal
   end
