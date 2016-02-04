dda98b0a75b65a20abd5d642ea61a66695c74be6
Joshua Zenn <jzenn@students.kennesaw.edu>
2016-02-04 14:16:02 +0000

    Fixed bad repo reference

--- scripts/gendocs.sh
+++ scripts/gendocs.sh
@@ -6,5 +6,5 @@ git init
 git checkout -b gh-pages
 git add -A
 git -c user.name='Travis CI' -c user.email='wordman05@gmail.com' commit -m init
-git push -f -q https://ARMmaster17:$GITHUB_API_KEY@github.com/PrincetonRidgeHA/HomeWeb-gh-pages gh-pages &2>/dev/null
+git push -f -q https://ARMmaster17:$GITHUB_API_KEY@github.com/PrincetonRidgeHA/HomeWeb gh-pages &2>/dev/null
 cd ..
