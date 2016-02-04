3e22c9fe76aecf5a80e667af97bbf6c0b9b18f39
Joshua Zenn <jzenn@students.kennesaw.edu>
2016-02-04 14:12:30 +0000

    Documentation automation

--- scripts/gendocs.sh
+++ scripts/gendocs.sh
@@ -1,3 +1,10 @@
 echo 'Generating documentation...'
 rdoc --main README.md
-echo 'Done!'
+echo 'Uploading to GitHub...'
+cd doc
+git init
+git checkout -b gh-pages
+git add -A
+git -c user.name='Travis CI' -c user.email='wordman05@gmail.com' commit -m init
+git push -f -q https://ARMmaster17:$GITHUB_API_KEY@github.com/PrincetonRidgeHA/HomeWeb-gh-pages gh-pages &2>/dev/null
+cd ..
